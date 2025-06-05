e78bfa9886462c6d299011e22ef4c4a48f0e82cc
kiril_tarasov <kirilltaras1792@yandex.ru>
2025-06-05 23:25:08 +0700

    Реализовать сериализатор для посчета уроков в курсе

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
@@ -7,6 +8,17 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons = SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ("title","description", "count_lessons")
+
+    def get_count_lessons(self, obj):
+        return Lesson.objects.filter(course=obj).count()
 
 
 class LessonSerializer(ModelSerializer):

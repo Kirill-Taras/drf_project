b3ac6804082b39313abcf202952b5fcca217192c
kiril_tarasov <kirilltaras1792@yandex.ru>
2025-06-06 16:32:15 +0700

    Вывести количество уроков курса и информацию по всем урокам курса одновременно

--- materials/serializer.py
+++ materials/serializer.py
@@ -3,25 +3,31 @@ from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
-
-class CourseSerializer(ModelSerializer):
+class LessonSerializer(ModelSerializer):
+    """Сериализатор для вывода информации об уроках."""
     class Meta:
-        model = Course
+        model = Lesson
         fields = "__all__"
 
 
+class CourseSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField( )
+
+    class Meta:
+        model = Course
+        fields = ["id", "title", "preview", "lessons_count"]
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+
 class CourseDetailSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
     count_lessons = SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ("title","description", "count_lessons")
+        fields = ("title","description", "count_lessons", "lessons")
 
     def get_count_lessons(self, obj):
-        return Lesson.objects.filter(course=obj).count()
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+        return obj.lessons.count()
